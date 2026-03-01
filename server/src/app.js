08fb0765bdbeed3de3e6efb05141afe864a027e7
Vijaya Sharma <vijayasharma@Vijayas-MacBook-Pro.local>
2026-02-28 22:05:10 -0500

    balcend intial server setup with Express and PostgreSQL

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+import express from express;
+import cors from "cors";
+import dotenv from "dotenv";
+
+import usersRoutes from "./routes/users.routes.js";
+import tasksRoutes from "./routes/tasks.routes.js";
+
+dotenv.config();// reads .env file automatically
+
+const app= express(); //creates server instance
+
+app.use(cors()); //allows frontend to talk to backend 5000
+app.use(express.json());
+
+app.use("/api/users", usersRoutes);
+app.use("/api/tasks", tasksRoutes);
+
+export default app;
